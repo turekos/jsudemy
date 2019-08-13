8599d1353f157edae2cde2e9fd294046e1bd1c90
Olek <tureksasha@gmail.com>
2019-08-13 18:29:15 +0200

    my first commit

---
+++ lesson1/js.js
@@ -0,0 +1,18 @@
+let persone = {
+    name: "Kola",
+    age: 23,
+    isMarried: true
+};
+
+
+// if (persone.isMarried == true) {
+//     console.log('no-no-no-no');
+// }
+
+let answer = confirm("Are you here?");
+
+console.log(answer);
+
+if (answer == true) {
+    console.log(persone);
+}
